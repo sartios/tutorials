89a5ec6f6bf598cd2f08ee051ccbfe67ee4390dd
ssavr <ssavr>
2016-04-18 16:14:18 +0300

    initial commit

---
+++ java/spotify-client/src/main/java/com/ssavr/tutorials/sportify/App.java
@@ -0,0 +1,13 @@
+package com.ssavr.tutorials.sportify;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
